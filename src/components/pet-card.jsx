21b93924d8af5e1ea7074f8803c47e1893e23063
RJ <13499408+raehart@users.noreply.github.com>
2022-02-08 21:31:58 -0800

    added text and comment re date formating on pet age for pet card

--- src/components/pet-card.jsx
+++ src/components/pet-card.jsx
@@ -15,7 +15,7 @@ function PetCard(props) {
   return (
     <div className="pet-card">
         <h2>{pet.name}</h2>
-        <div> Age: {Date.now()-pet.birth}</div>
+        <div> Age: {Date.now()-pet.birth} milliseconds old</div>
         <img src={pet.photo} alt={pet.alttext} />
   
       <PetStats/>
