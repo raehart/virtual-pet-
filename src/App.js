80026dfb8b75b7a2545c8ecca997924c452b1b49
RJ <13499408+raehart@users.noreply.github.com>
2022-02-10 12:17:47 -0800

    renamed petCard to petDetail

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-import PetCard from './components/pet-card';
+import PetDetail from './components/pet-detail';
 
-function App() {
+function App(props) {
+  const eggs = [
+    {
+      name: 'Meeps',
+      photo: "http://placekitten.com/g/300/300",
+      alttext: "a placeholder kitten"
+    }, {
+      name: 'Lawl',
+      photo: "http://placekitten.com/g/300/301",
+      alttext: "a placeholder kitten"
+    }, {
+      name: 'Chirp',
+      photo: "http://placekitten.com/g/301/300",
+      alttext: "a placeholder kitten"
+    }
+  ]
+
+
   return (
     <div className="App">
       <h1>My Vitural Pet</h1>
-      <PetCard/>
+      <PetDetail/>
     </div>
   );
 }
--- src/components/pet-detail.jsx
+++ src/components/pet-detail.jsx
@@ -1,9 +1,9 @@
-import './pet-card.css';
+import './pet-detail.css';
 import PetStats from './pet-stats';
 import moment from 'moment';
 
 //birth miliseconds generarted when project started- with Date.now - - feature plan is to add on adpotion - when pet select button is clicked
-function PetCard(props) {
+function PetDetail(props) {
   const pet = {
     name: 'Meeps',
     birth: 1644382990446,
@@ -12,16 +12,14 @@ function PetCard(props) {
   }
 
   const petAge = moment(pet.birth, "unix").fromNow();
-
-
   return (
-    <div className="pet-card">
-        <h2>{pet.name}</h2>
-        <div> Hatched {petAge} </div>
-        <img src={pet.photo} alt={pet.alttext} />
+    <div className="pet-detail">
+      <h2>{pet.name}</h2>
+      <div>Hatched {petAge}</div>
+      <img src={pet.photo} alt={pet.alttext} />
       <PetStats/>
     </div>
   );
 }
 
-export default PetCard;
+export default PetDetail;
